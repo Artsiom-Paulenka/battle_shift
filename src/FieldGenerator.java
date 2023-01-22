0cf4d4e6c08d4b001cb2cbbd29597ce9029a5ea9
Artsiom Paulenka <pavlenkoartem611@gmail.com>
2023-01-22 15:14:19 +0100

    Commit

--- src/FieldGenerator.java
+++ src/FieldGenerator.java
@@ -11,35 +11,59 @@ public class FieldGenerator {
         this.width = width;
     }
 
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
     public int[][] generate() {
         Random random = new Random();
         int[][] field = new int[length][width];
 
         int countOfShips = 0;
-        while (countOfShips < 100) {
+        while (countOfShips < 50) {
+
+
             int a = random.nextInt(0, length);
             int b = random.nextInt(0, width);
 
-            if (field[a][b] == 0) {
-                field[a][b] = 1;
-                countOfShips++;
+//            for (int i = 0; i < field.length; i++) {
+//                for (int j = 0; j < field[i].length; j++) {
+
+                    //if (field[i][j] == 2) {
+                    //    field[i][j] == 2;
+                    //    j++;
+                    //}
+
+
+                    if (field[a][b] == 0) {
+                        field[a][b] = 1;
+                        countOfShips++;
+                    }
+                    if (field[a][b] == 2) {
+                        field[a + 1][b + 1] = 2;
+                    }
+
+                }
+                return field;
             }
         }
 
 
-        //for (int i = 0; i < 100; i++) {
-        //    int a = random.nextInt(0, length);
-        //    int b = random.nextInt(0, width);
+//for (int i = 0; i < 100; i++) {
+//    int a = random.nextInt(0, length);
+//    int b = random.nextInt(0, width);
 //
-        //    if (field[a][b] == 1) {
-        //        i--;
-        //    }
-        //    else {
-        //        field[a][b] = 1;
-        //    }
-        //}
+//    if (field[a][b] == 1) {
+//        i--;
+//    }
+//    else {
+//        field[a][b] = 1;
+//    }
+//}
 //
 
-        return field;
-    }
-}
+
--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,14 @@ public class Main {
         FieldPrinter fieldPrinter = new FieldPrinter(10);
         fieldPrinter.printField(field);
 
-        Game game = new Game(field);
+        Game game = new Game(field,10,10);
+
         game.start();
+        //fieldPrinter.printField(field);
     }
 }
 
-      // int a = 5;
-      // int[] b = new int[10];
-      // int [][] c = new int [5][8];
 
-      //b[0] = 5;
-      //b[3] = 5;
-      //b[6] = 5;
-      //b[7] = 5;
-      //b[4] = 5;
-      //b[9] = 5;
-
-      //c[1][2] = 6;
-      // System.out.println(Arrays.toString(b));
-      // System.out.println(Arrays.deepToString(c));
-
-      // int sum = 0;
-      // for (int i = 0; i <10; i++) {
-      //     sum += b[i];
-      // }
-      // System.out.println(sum);}
-//}
 
 
 
